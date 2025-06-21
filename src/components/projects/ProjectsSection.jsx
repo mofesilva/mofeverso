21ea3b08ec5b5058fc62e0f16498797cfc28aa8f
mofesilva <mofesilva.dev@gmail.com>
2025-06-21 19:32:36 -0300

    Criação dos componentes 'Projects section'.

---
+++ src/components/projects/ProjectsSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProjectsSection() {
+    return (
+        <div>
+            <h1>ProjectsSection</h1>
+        </div>
+    )
+}
+
+export default ProjectsSection;
