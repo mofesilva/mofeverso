bc427f67caf9de2916f5ef09f56688e5835f94b0
mofesilva <mofesilva.dev@gmail.com>
2025-06-21 19:31:45 -0300

    Bio atualizada com texto corrigido e revisado.

--- src/components/profile/ProfileBio.jsx
+++ src/components/profile/ProfileBio.jsx
@@ -6,7 +6,7 @@ function ProfileBio() {
         <div id="bio" className="font-bold w-full lg:w-1/2 justify-self-center">
             <SectionTitle title="Bio" />
             <p className="text-md font-league-spartan text-justify font-light text-white pt-2 pr-4 pl-4">
-                Aprendendo a programar e a criar coisas incríveis. Aprendendo a programar e a criar coisas incríveis. Aprendendo a programar e a criar coisas incríveis. Aprendendo a programar e a criar coisas incríveis. Aprendendo a programar e a criar coisas incríveis
+                Apaixonado por criar coisas incríveis e resolver problemas. Vejo nessas duas áreas o núcleo das minha habilidades. Estudar é parte fundamental nessa jornada. Gosto de adquirir e compartilhar conhecimento e estou sempre em busca de crescer e explorar novos caminhos. Curioso por natureza, encaro cada projeto como uma oportunidade de inovar e fazer a diferença.
             </p>
         </div>
     )
