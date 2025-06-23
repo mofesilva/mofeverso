db9a381b987bda2139e59e1b127bc2d9f82acba8
Moisés Silva <mofesilva.dev@gmail.com>
2025-06-22 23:01:57 -0300

    Ajustes na largura do componente

--- src/components/projects/ProjectsSection.jsx
+++ src/components/projects/ProjectsSection.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import SectionTitle from '../SectionTitle';
 
 function ProjectsSection() {
     return (
-        <div>
-            <h1>ProjectsSection</h1>
+        <div className="h-screen w-full lg:w-1/2 mt-4 justify-self-center">
+            <SectionTitle title="Projetos" />
         </div>
     )
 }
